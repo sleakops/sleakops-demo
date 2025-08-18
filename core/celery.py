b338d223fba282ee59d714a479f4d3b2340ec16f
Karim Brizuela <karim090@gmail.com>
2025-08-18 16:21:10 -0300

    Update celery.py

--- core/celery.py
+++ core/celery.py
@@ -9,6 +9,14 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.local")
 
 app = Celery("core")
 
+# Si la variable de entorno para SQS está definida, la cargamos
+if os.getenv("CELERY_BROKER_URL") == "sqs://" and os.getenv("CELERY_BROKER_TRANSPORT_OPTIONS"):
+    broker_transport_options = json.loads(os.getenv("CELERY_BROKER_TRANSPORT_OPTIONS"))
+    app.conf.broker_transport_options = broker_transport_options
+else:
+    # Si no estamos usando SQS, entonces no configuramos las opciones de SQS
+    app.conf.broker_transport_options = {}
+
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys

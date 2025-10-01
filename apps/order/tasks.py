2e2ebe954f6e2c42846a2b51e652610bdbae26cd
matiar <matiroson@gmail.com>
2025-10-01 17:23:55 -0300

    fix: order task demo

--- apps/order/tasks.py
+++ apps/order/tasks.py
@@ -5,6 +5,6 @@ from .models import Order
 
 @shared_task
 def order_process(order_id):
-    order = Order.objects.get(id=order)
+    order = Order.objects.get(id=order_id)
     print(f'Process order number # {order.id}')
     print(f"User {order.user.username} has placed an order with status {order.status}")
